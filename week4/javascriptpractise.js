fcd6503052baeafe178c75a0862dfca52ce9447c
Salahuddin Qureshi <146633538+Salahuddin-Qureshi@users.noreply.github.com>
2024-03-03 18:11:46 +0500

    Add files via upload

---
+++ week4/javascriptpractise.js
@@ -0,0 +1,20 @@
+// console.log("Hello","Salahuddin")
+// isTrue = true;
+// console.log(!isTrue)
+// var num = 10;
+// var name = ""+num;
+// console.log(typeof(name) + " "+ name, " ", typeof(num), " ", num);
+
+let array = [1,2,3,4,5,6]
+console.log(array.reverse())
+const person = {
+    name : "Salahuddin",
+    fname: "Badaruddin",
+    cast: "Qureshi"
+};
+console.log(person.name)
+console.log(person.cast)
+showAlert()
+function showAlert(){
+    alert("This is an Alert!")
+}
